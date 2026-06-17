8caab6ef05b3578577a7ba16735cdb1c51f227a3
manik-nav <maniknvrthn@gmail.com>
2026-06-17 14:35:23 +0700

    setup prod config

--- web/src/app/services/user.service.ts
+++ web/src/app/services/user.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 import { User } from '../models/user.model';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
   private readonly http = inject(HttpClient);
-  private readonly apiUrl = '/api/users';
+  private readonly apiUrl = `${environment.apiUrl}/users`;
 
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.apiUrl);
---
+++ web/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://test.api.intillo.com/api'
+};
